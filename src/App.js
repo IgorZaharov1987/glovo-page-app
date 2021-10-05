756ae5ba3292e24d1592f6fce391f7b1d3d76d72
Igor Zaharov <igorzaharov1987@gmail.com>
2021-10-05 16:43:33 +0300

    implementing i18next translation library

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 /* eslint-disable */
+import logo from './logo.svg';
 //import './App.css';
 import Header from './components/header/header.js';
 import Main from "./components/main/main";
 import Slider from './components/slider/slider.js';
 import Footer from './components/footer/footer.js';
-
 
 function App() {
   return (
